1e6c615639f0412e52d749fcf7b72d35f177321a
Danny Brown <dannybrown37@gmail.com>
2023-07-22 19:26:15 -0400

    add python file output; add ruff; tweak config

--- src/localeet/__main__.py
+++ src/localeet/__main__.py
@@ -1,7 +1,43 @@
-#!/usr/bin/env python3.9
+"""
+Entry point for CLI. Define and parse CLI arguments.
+"""
+from pathlib import Path
 
-from localeet.get_leetcode_problems import run_series
+import click
+
+from localeet.get_leetcode_problem import run
+
+DIFFICULTY_MAP = {
+    'easy': 1,
+    'medium': 2,
+    'hard': 3,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+}
 
 
-def main():
-    run_series()
+@click.command()
+@click.option(
+    '--max_difficulty', '--max', '-m',
+    help='Max difficulty allowed',
+    type=click.Choice(list(DIFFICULTY_MAP.keys())),
+)
+@click.option(
+    '--min_difficulty', '--min', '-m',
+    help='Min difficulty allowed',
+    type=click.Choice(list(DIFFICULTY_MAP.keys())),
+)
+@click.option(
+    '--output_path', '--path', '-o',
+    help='Output path for code file created. Default is cwd. '
+         'Will create new directories as needed',
+    default='.',
+)
+def main(max_difficulty: str, min_difficulty: str, output_path: str) -> None:
+    """Entry point for CLI. Parse CLI arguments."""
+    max_difficulty = DIFFICULTY_MAP.get(max_difficulty, 3)
+    min_difficulty = DIFFICULTY_MAP.get(min_difficulty, 1)
+    output_path = Path(output_path)
+
+    run(max_difficulty, min_difficulty, output_path)
