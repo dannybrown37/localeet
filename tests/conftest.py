ee00c31e06a7cceba1cb1cc9a8c9541991baa203
Danny Brown <dannybrown37@gmail.com>
2023-07-23 08:49:35 -0400

    fix: ran ruff on tests/; all rules don't necessarily apply so won't add to CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,17 @@
 import json
+from pathlib import Path
 
 import pytest
 
 
 class MockValue:
     """Allows any of a datatype to pass comparison within dictionary"""
-    def __init__(self, name='MOCK_VALUE', data_type=None, validator=None):
+    def __init__(
+            self,
+            name='MOCK_VALUE',
+            data_type=None,
+            validator=None,
+        ) -> None:
         self.name = name
         self.data_type = data_type
         self.validator = validator
@@ -21,47 +27,38 @@ class MockValue:
     def __ne__(self, item):
         return not self.__eq__(item)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'<ANY_{self.name}>'
 
 
 @pytest.fixture(scope='session')
-def any_int():
+def any_int() -> MockValue:
     return MockValue('INT', int)
 
 
 @pytest.fixture(scope='session')
-def any_str():
+def any_str() -> MockValue:
     return MockValue('STR', str)
 
 
 @pytest.fixture(scope='session')
-def any_json_str():
+def any_json_str() -> MockValue:
     return MockValue('JSON_STR', str, lambda x: json.loads(x))
 
 
 @pytest.fixture(scope='session')
-def two_sum_start():
-    return MockValue(
-        'TWO_SUM_DESCRIPTION',
-        str,
-        lambda x: x.startswith('Given an array of integers'),
-    )
-
-
-@pytest.fixture(scope='session')
-def two_sum_details_json():
-    with open('tests/data/two_sum_details.json', 'r') as f:
+def two_sum_details_json() -> dict:
+    with Path('tests/data/two_sum_details.json').open() as f:
         return json.load(f)
 
 
 @pytest.fixture(scope='session')
-def two_sum_essentials():
-    with open('tests/data/two_sum_essentials.json', 'r') as f:
+def two_sum_essentials() -> dict:
+    with Path('tests/data/two_sum_essentials.json').open() as f:
         return json.load(f)
 
 
 @pytest.fixture(scope='session')
 def sample_two_sum_python_file():
-    with open('tests/data/two_sum.py', 'r') as f:
+    with Path('tests/data/two_sum.py').open() as f:
         return f.read()
